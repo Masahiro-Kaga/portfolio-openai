364dc4c73aeeed9468159691ebc87ea8ad25ce10
masa <masahiro41802891@gmail.com>
2023-10-30 20:50:38 -0700

    Set up basic Node.js server and environment variable

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.SERVER_PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server is running on PORT ${port}`));
