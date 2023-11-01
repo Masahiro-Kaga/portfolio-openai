089735c1d6249d307371654de30d07fe917150ab
masa <masahiro41802891@gmail.com>
2023-10-31 22:24:34 -0700

    Implement API test of GET and POST endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,55 @@
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState<string>("");
+  const axiosGetTestHandler = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/getTest");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const axiosPostTestHandler = async () => {
+    console.log(username);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/postTest",
+        {
+          username,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="text-3xl">
-      Starting point
+    <div className="flex flex-col w-64">
+      <div className="text-3xl">Starting point</div>
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        className="border-2"
+      ></input>
+      <button
+        onClick={() => axiosGetTestHandler()}
+        className="border-2 bg-sky-600"
+      >
+        Axios Get Test
+      </button>
+      <button
+        onClick={() => axiosPostTestHandler()}
+        className="border-2 bg-rose-700"
+      >
+        Axios Post Test
+      </button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,4 +8,15 @@ const port = process.env.SERVER_PORT;
 app.use(cors());
 app.use(express.json());
 
+app.get("/getTest", (req,res) => {
+  console.log("Get Test Request");
+  res.json({message:"Hello Get test"});
+});
+
+app.post("/postTest", (req,res) => {
+  console.log("Post Test Request");
+  console.log(req.body);
+  res.json("Post Test");
+});
+
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
