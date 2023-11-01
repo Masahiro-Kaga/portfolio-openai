06f4677839a8cb7753305dcd90253a10f1038cbd
masa <masahiro41802891@gmail.com>
2023-11-01 10:14:08 -0700

    Serve built React files successfully using Express static middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env'});
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const connectDB = require('./db');
@@ -25,5 +26,17 @@ app.post("/postTest", (req,res) => {
   user.save();
   res.json("Post Test");
 });
+
+// if (process.env.NODE_ENV === "production") {
+  console.log("Node_env??");
+  console.log(process.env.NODE_ENV);
+
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+// }
 
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
